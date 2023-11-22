16cb33339e6a71bdea78f4cd37b3c394f7a7da1c
SatyamKumar <kumarsatyam04.2000@gmail.com>
2023-11-22 23:37:15 +0530

    fetch data from tmdb

---
+++ src/pages/[id].js
@@ -0,0 +1,39 @@
+import MovieDetails from "@/components/MovieDetails";
+import { useState } from "react";
+
+const MovieDetailPage = ({ movie }) => {
+  const [showPlayer, setShowPlayer] = useState(false);
+
+  const trailerIndex = movie.videos.results.findIndex(
+    (element) => element.type === "Trailer"
+  );
+
+  const trailerURL = `https://www.youtube.com/watch?v=${movie.videos?.results[trailerIndex]?.key}`;
+
+  return (
+    <div>
+      <MovieDetails
+        movie={movie}
+        showPlayer={showPlayer}
+        setShowPlayer={setShowPlayer}
+        trailerURL={trailerURL}
+      />
+    </div>
+  );
+};
+
+export async function getServerSideProps(context) {
+  const { id } = context.query;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US&append_to_response=videos`
+  ).then((response) => response.json());
+
+  return {
+    props: {
+      movie: request,
+    },
+  };
+}
+
+export default MovieDetailPage;
